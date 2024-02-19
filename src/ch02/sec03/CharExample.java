5e9f0b2cbb4802a7a88619a27bc30448b697ecf9
huitopia <huitopia27@gmail.com>
2024-02-19 16:17:56 +0900

    char 타입의 변수 예제

---
+++ src/ch02/sec03/CharExample.java
@@ -0,0 +1,15 @@
+package ch02.sec03;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c2 = 65;
+        char c3 = '가';
+        char c4 = 44032;
+
+        System.out.println(c1); // A
+        System.out.println(c2); // A
+        System.out.println(c3); // 가
+        System.out.println(c4); // 가
+    }
+}
