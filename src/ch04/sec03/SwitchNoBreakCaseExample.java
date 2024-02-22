5dd2cf94b587c4d3de99b2dbc0e43a77bba5def0
huitopia <huitopia27@gmail.com>
2024-02-22 14:47:28 +0900

    switch break가 없으면

---
+++ src/ch04/sec03/SwitchNoBreakCaseExample.java
@@ -0,0 +1,18 @@
+package ch04.sec03;
+
+public class SwitchNoBreakCaseExample {
+    public static void main(String[] args) {
+        int time = (int) (Math.random() * 4) + 8;
+        System.out.println("현재시간" + time);
+        switch (time) {
+            case 8:
+                System.out.println("출근");
+            case 9:
+                System.out.println("회의");
+            case 10:
+                System.out.println("업무");
+            default:
+                System.out.println("외근");
+        }
+    }
+}
