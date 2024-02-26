677203996e0a3541650c41d9ff700c82389e69a7
huitopia <huitopia27@gmail.com>
2024-02-26 11:03:13 +0900

    NullPointerException

---
+++ src/ch05/sec04/NullPointerExceptionExample.java
@@ -0,0 +1,12 @@
+package ch05.sec04;
+
+public class NullPointerExceptionExample {
+    public static void main(String[] args) {
+        int[] intArray = null;
+//        intArray[0] = 10;
+        System.out.println("intArray = " + intArray);
+
+        String str = null;
+//        System.out.println("str.length() = " + str.length());
+    }
+}
