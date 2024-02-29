292564d016d046a12726584c34330102e7cebea0
huitopia <huitopia27@gmail.com>
2024-02-29 15:20:12 +0900

    생성자 선언

---
+++ src/cs06/sec07/exam01/Car.java
@@ -0,0 +1,6 @@
+package cs06.sec07.exam01;
+
+public class Car {
+    Car(String model, String color, int maxSpeed) {
+    }
+}
---
+++ src/cs06/sec07/exam01/CarExample.java
@@ -0,0 +1,7 @@
+package cs06.sec07.exam01;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car("투싼", "black", 250);
+    }
+}
