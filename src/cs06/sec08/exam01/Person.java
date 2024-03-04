04ba7d7d60a047aff4a712cd467737d963e11083
huitopia <huitopia27@gmail.com>
2024-03-04 15:09:31 +0900

    메소드 선언 호출 예제

---
+++ src/cs06/sec08/exam01/Person.java
@@ -0,0 +1,13 @@
+package cs06.sec08.exam01;
+
+public class Person {
+    String name;
+
+    void run(String name) {
+        System.out.println(name + "이 달립니다.");
+    }
+
+    void walk(String name) {
+        System.out.println(name + "이 걸어갑니다.");
+    }
+}
---
+++ src/cs06/sec08/exam01/PersonExample.java
@@ -0,0 +1,10 @@
+package cs06.sec08.exam01;
+
+public class PersonExample {
+    public static void main(String[] args) {
+        Person myPerson = new Person();
+        String name = "hone";
+        myPerson.run(name);
+    }
+
+}
