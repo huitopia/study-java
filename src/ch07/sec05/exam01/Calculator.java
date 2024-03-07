f9f37259efcf166a2947376b32c771844d17dcdd
huitopia <huitopia27@gmail.com>
2024-03-07 22:07:32 +0900

    method overriding

---
+++ src/ch07/sec05/exam01/Calculator.java
@@ -0,0 +1,10 @@
+package ch07.sec05.exam01;
+
+public class Calculator {
+    public double areaCircle(double r) {
+        // 메소드 선언
+        System.out.println("Calculator 객체의 areaCircle() 실행");
+        return 3.14159 * r * r;
+    }
+}
+
