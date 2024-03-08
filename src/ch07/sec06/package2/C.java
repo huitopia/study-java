780cfa926ca70b0edf632bd033a34bb2b99416bc
huitopia <huitopia27@gmail.com>
2024-03-08 17:15:56 +0900

    protected 접근 제한자

---
+++ src/ch07/sec06/package2/C.java
@@ -0,0 +1,14 @@
+package ch07.sec06.package2;
+
+import ch07.sec06.package1.A;
+
+public class C {
+
+    public void method() {
+        /* 다른 패키지에 있어서 접근 못함
+        A a = new A();
+        a.field = "value";
+        a.method();
+         */
+    }
+}
---
+++ src/ch07/sec06/package2/D.java
@@ -0,0 +1,22 @@
+package ch07.sec06.package2;
+
+import ch07.sec06.package1.A;
+
+public class D extends A {
+    public D() {
+        super();
+    }
+
+    public void method1() {
+        this.field = "value";
+        this.method();
+    }
+
+    public void method2() {
+        /* 직접 객체 생성해서 사용 금지
+        A a = new A();
+        a.field = "value";
+        a.method();
+         */
+    }
+}
