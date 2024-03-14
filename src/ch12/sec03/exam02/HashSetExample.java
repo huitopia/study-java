2204ad50939eda917fb131fde4a519c60351b56a
huitopia <huitopia27@gmail.com>
2024-03-14 15:16:06 +0900

    HashSet

---
+++ src/ch12/sec03/exam02/HashSetExample.java
@@ -0,0 +1,25 @@
+package ch12.sec03.exam02;
+
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        // HashSet : 동등 객체를 중복 저장하지 않는다.
+        // HashSet java util 패키지에 있으므로 import
+        // HashSet 컬렉션 생성
+        HashSet hashSet = new HashSet();
+        // HashSet에 Student 객체 생성
+        Student student1 = new Student(1, "김가네");
+        // HashSet에 Student 객체 저장
+        hashSet.add(student1);
+        System.out.println("저장된 객체 수 : " + hashSet.size()); // 1
+
+        Student student2 = new Student(1, "김가네");
+        hashSet.add(student2);
+        System.out.println("저장된 객체 수 : " + hashSet.size()); // 1
+
+        Student student3 = new Student(2, "김가네");
+        hashSet.add(student3);
+        System.out.println("저장된 객체 수 : " + hashSet.size()); // 2
+    }
+}
