a0f0203d2c76db6e59f6ef7845ba7d6ee34e9141
huitopia <huitopia27@gmail.com>
2024-03-18 17:21:57 +0900

    Wrapper Class 상위 객체 변환

--- src/ch12/sec00/WrapperClass03.java
+++ src/ch12/sec00/WrapperClass03.java
@@ -5,8 +5,8 @@ public class WrapperClass03 {
         // 기본 > 참조
         int a = 5;
         Integer b = 5; // auto boxing
-        Number c = 5; // auto boxing
-        Object d = 5; // auto boxing
+        Number c = a; // auto boxing
+        Object d = a; // auto boxing
 
         Integer e = null; // ok
 //        int f = null; // 값 자체가 없기 때문에 null 사용 불가능
