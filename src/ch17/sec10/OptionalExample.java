1928281b4fb87a946f39139c8d2e3dfd945e731d
huitopia <huitopia27@gmail.com>
2024-03-22 14:46:25 +0900

    Optional

--- src/ch17/sec10/OptionalExample.java
+++ src/ch17/sec10/OptionalExample.java
@@ -9,9 +9,10 @@ public class OptionalExample {
         List<Integer> list = new ArrayList<>();
 
         OptionalDouble optional1 = list.stream()
+                // mapToInt : 참조 타입 Integer ➡️ 기본 타입 int
                 .mapToInt(Integer::intValue)
                 .average();
-        if (optional1.isPresent()) {
+        if (optional1.isPresent()) { // 값이 있으면
             System.out.println("1 average : " + optional1.getAsDouble());
         } else {
             System.out.println("1 average : 0.0");
