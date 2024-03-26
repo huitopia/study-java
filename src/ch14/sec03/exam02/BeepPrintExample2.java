0fc65f465f1eeece6f063de480c034b6b325e9a7
huitopia <huitopia27@gmail.com>
2024-03-26 11:00:40 +0900

    Thread 람다식

---
+++ src/ch14/sec03/exam02/BeepPrintExample2.java
@@ -0,0 +1,27 @@
+package ch14.sec03.exam02;
+
+import java.awt.*;
+
+public class BeepPrintExample2 {
+    public static void main(String[] args) {
+        // Lambda
+        Thread thread = new Thread(() -> {
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            for (int i = 0; i < 5; i++) {
+                toolkit.beep();
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+            }
+        });
+        thread.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
