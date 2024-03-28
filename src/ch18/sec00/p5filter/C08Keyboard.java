02ab86c597de7ad8b336e265ffe43a5ebc2ac8e9
huitopia <huitopia27@gmail.com>
2024-03-28 12:34:31 +0900

    수업 예제 - BufferedReader

---
+++ src/ch18/sec00/p5filter/C08Keyboard.java
@@ -0,0 +1,21 @@
+package ch18.sec00.p5filter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class C08Keyboard {
+    public static void main(String[] args) throws IOException {
+        InputStream keyboard = System.in;
+        InputStreamReader isr = new InputStreamReader(keyboard);
+        BufferedReader br = new BufferedReader(isr);
+        while (true) {
+            System.out.print("입력 > ");
+            String line = br.readLine();
+            if (line.equals("exit")) break;
+            System.out.println("출력 ➡️ " + line);
+        }
+        br.close();
+    }
+}
